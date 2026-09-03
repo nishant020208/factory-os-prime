ec97c9cbcd9be60c089e95560db8ca33994f08f8
nishant020208 <nishant020208@gmail.com>
2026-09-03 20:43:59 +0530

    Declare favicon links in document head

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -184,6 +184,8 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       },
     ],
     links: [
+      { rel: "icon", type: "image/png", href: "/favicon.png" },
+      { rel: "apple-touch-icon", type: "image/png", href: "/favicon.png" },
       { rel: "stylesheet", href: appCss },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
